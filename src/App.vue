77ca04c5198a1e995c371e0f4525ebfae5396664
ycremar <xieyc95@gmail.com>
2019-07-18 21:40:47 -0500

    add tracker

--- src/App.vue
+++ src/App.vue
@@ -3,41 +3,50 @@ div#app
     el-container
         el-header
             arkplanner-header
-        el-main
-            el-row(style="display: flex; flex-wrap: wrap; justify-content: center;")
-                div(style="margin: 10px")
-                    el-switch(v-model='extra_outc_bool' active-color='#13ce66' inactive-color='#e0e0e0' active-text='计算合成副产物')
-                div(style="margin: 10px")
-                    el-switch(v-model='exp_demand_bool' active-color='#13ce66' inactive-color='#e0e0e0' active-text='大量需求经验')
-                div(style="margin: 10px")
-                    el-switch(v-model='gold_demand_bool' active-color='#13ce66' inactive-color='#e0e0e0' active-text='大量需求龙门币')
-            el-row(style="display: flex; flex-wrap: wrap; justify-content: center;")
-                el-card(v-for="material in materialList" v-bind:key='material.id' :class='`rarity-` + material.rarity')
-                    div(slot='header' class='clearfix' style='display: flex; align-items: center;')
-                        img(:src='material.iconPath')
-                        span(style='margin-left: 10px; font-size: 18px;') {{material.name}}
-                    div
-                        div(style="display: inline-block;") 需要#[el-input-number(v-model='material.required' :min='0' size='small') ]
-                        div(style="display: inline-block;") 已有#[el-input-number(v-model='material.owned'  :min='0' size='small')]
+        el-container
+            el-aside(width="200px")
+                div(style="margin: 20px")
+                    el-switch(v-model='extra_outc_bool' active-color='#0abab5' active-text='计算合成副产物' inactive-color='#e0e0e0') 
+                div(style="margin: 20px")
+                    el-switch(v-model='exp_demand_bool' active-color='#0abab5' inactive-color='#e0e0e0' active-text='大量需求经验')
+                div(style="margin: 20px")
+                    el-switch(v-model='gold_demand_bool' active-color='#0abab5' inactive-color='#e0e0e0' active-text='大量需求龙门币')
+                div(style="margin: 20px")
+                    el-button(type="primary" plain style="width:143px;height:38px;color:#14314f;" id="importOrExport" @click='toggleImportOrExport') 导入/导出
+                div(style="margin: 20px")
+                    el-button(type="primary" plain style="width:143px;height:38px;color:#14314f;" id="fullyRequired" @click='applyFullyRequired') 载入全需求
+                div(style="margin: 20px")
+                    el-button(type="primary" plain icon="el-icon-money" style="color:#14314f;" id="costume" @click='costume') 众筹站长女装
+                div(style="margin: 20px")
+                    el-button(type="success" id="analyze" :loading=isLoading @click='analyze') 计算规划
 
-        el-footer
-            arkplanner-footer
-        el-button(type="primary" icon="el-icon-data-analysis" circle id="analyze" @click='analyze')
-        el-button(icon="el-icon-more" circle id="importOrExport" @click='toggleImportOrExport')
+            el-container
+                el-main
+                    el-row(style="display: flex; flex-wrap: wrap; justify-content: center;")
+                        el-card(v-for="material in materialList" v-bind:key='material.id' :class='`rarity-` + material.rarity')
+                            div(slot='header' class='clearfix' style='display: flex; align-items: center;')
+                                img(:src='material.iconPath')
+                                span(style='margin-left: 10px; font-size: 18px;') {{material.name}} 
+                            div
+                                div(style="display: inline-block;") 需要#[el-input-number(v-model='material.required' :min='0' size='small') ]
+                                div(style="display: inline-block;") 已有#[el-input-number(v-model='material.owned'  :min='0' size='small')]
+                el-footer
+                    arkplanner-footer
 
         el-dialog(:title='`预计需要体力: ` + cost' :visible.sync="resultVisible" width="80%")
             el-tabs(v-model="activeResult")
                 el-tab-pane(label='关卡列表' name='stagesList')
-                    p() 共计获得#[b 龙门币] {{gold}} 以及价值 {{exp}} EXP 的#[b 录像带]
+                    p() 共计获得#[b 龙门币 {{gold}}] 以及价值 #[b {{exp}} EXP 的录像带]
                     p(v-for='_stage in stages') #[b {{_stage.stage}}]: {{_stage.count}} 次，获得 
                         span(v-for='key in Object.keys(_stage.items)') #[b {{key}}]({{_stage.items[key]}}) 
                 el-tab-pane(label='合成列表' name='syntheses')
-                    p(v-if="extra_outc_bool") 由于考虑了合成副产物的产出，可能会出现提示合成不需要的材料，在需求量较小的时候建议关闭合成副产物
+                    p(v-if="extra_outc_bool") 由于考虑了合成副产物的产出，计算结果中可能会合成不需要的材料。在需求量较小的时候建议关闭合成副产物
+                    p() 合成共计消耗#[b 龙门币 {{gcost}}]
                     p(v-for="synthesis in syntheses") #[b {{synthesis.target}}]({{synthesis.count}}): 
                         span(v-for="key in Object.keys(synthesis.materials)") #[b {{key}}]({{synthesis.materials[key]}}) 
                 el-tab-pane(label='素材价值' name='itmvalues')
                     p(v-for='group in itmvalues') 素材等级#[b {{group.level}}]: <br/>
-                        span(v-for='value in group.values') {{value.item}}(#[b {{value.value}}]) 
+                        span(v-for='item in group.items') {{item.name}}(#[b {{item.value}}]) 
         
         el-dialog(title='导入/导出' :visible.sync="ioVisible" width="80%")
             el-tabs(v-model="activeIO")
@@ -50,17 +59,34 @@ div#app
                     div(style="margin: 10px")
                         el-input(v-model='materialExportJSON' type='textarea' :rows='5' readonly=true)
 
+        el-dialog(title='红白女装众筹' :visible.sync="cosVisible" width="80%")
+            div(style="margin: 20px") 
+                p() 用户量激增，服务器月费上涨，企鹅数据的运营陷入了危机。站长红白为了保护喜爱的企鹅数据而决定成为偶像...
+            el-row(style="display: flex; flex-wrap: wrap; justify-content: center;")
+                div(style="margin: 20px") 
+                    el-button(type="primary" plain @click='costume') 我知道了,会准备好钱包。
+
+
 
 </template>
 
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import VueAnalytics from 'vue-analytics';
 import ArkplannerHeader from './components/Header.vue';
 import ArkplannerFooter from './components/Footer.vue';
 import HelloWorld from './components/HelloWorld.vue';
+import * as fullyJSON from './assets/fully_required.json';
 
-import getMaterialsList, { Material } from './Material';
+
+const VueAnalytics = require('vue-analytics').default;
+Vue.use(VueAnalytics, {
+  id: 'UA-144114297-1',
+  autoTracking: {
+    screenview: true
+  }
+})
 
 const assemblePostData = (materialList: Material[], extra_outc_bool: any, exp_demand_bool: any, gold_demand_bool: any) => {
     const obj = {
@@ -91,28 +117,46 @@ const assemblePostData = (materialList: Material[], extra_outc_bool: any, exp_de
         ArkplannerFooter,
   },
 })
+
 export default class App extends Vue {
-    public materialList = getMaterialsList().sort((a: Material, b: Material) =>  b.rarity - a.rarity);
+    public materialList = getMaterialsList().sort((a: Material, b: Material) =>  b.id - a.id);
     public materialJSON = '';
     public materialExportJSON = '';
     public cost = 0;
+    public gcost = 0;
     public gold = 0;
     public exp = 0;
 
     public ioVisible = false;
+    public cosVisible = false;
     public activeIO = 'importJSON';
     public extra_outc_bool = false;
-    public exp_demand_bool = true;
+    public exp_demand_bool = false;
     public gold_demand_bool = true;
 
     public resultVisible = false;
     public activeResult = 'stagesList';
+    public isLoading = false;
 
     public stages: any = [];
     public syntheses: any = [];
     public itmvalues: any = [];
 
+    public costume() {
+        if (!this.cosVisible){
+            this.$ga.event({
+                eventCategory: 'costume',
+                eventAction: 'click',
+            });
+        }
+        this.cosVisible = !this.cosVisible
+    }
+
     public applyJSON() {
+        this.$ua.event({
+            eventCategory: 'import',
+            eventAction: 'click',
+            })
         const materialImportList = JSON.parse(this.materialJSON);
         let required_map: { [key: string]: number; } = {};
         let owned_map: { [key: string]: number; } = {};
@@ -131,13 +175,41 @@ export default class App extends Vue {
         this.ioVisible = false;
     }
 
+    public applyFullyRequired() {
+        Vue.analytics.event({
+            eventCategory: 'fullyRequired',
+            eventAction: 'click',
+            })
+        const materialImportList = (fullyJSON as any).default;
+        let required_map: { [key: string]: number; } = {};
+        let owned_map: { [key: string]: number; } = {};
+
+        for (let materialImport of materialImportList) {
+            required_map[materialImport.name] = materialImport.need;
+            owned_map[materialImport.name] = materialImport.have;
+        }
+
+        for (let material of this.materialList) {
+            if (material.name in owned_map) {
+                material.owned = owned_map[material.name];
+                material.required = required_map[material.name];
+            }
+        }
+    }
+
     public analyze() {
+        this.isLoading = true;
+        Vue.ga.event({
+            eventCategory: 'analyze',
+            eventAction: 'click',
+            })
         const req = new XMLHttpRequest();
         req.open('POST', 'plan/', true);
         req.setRequestHeader('Content-Type', 'application/json');
         req.onreadystatechange = () => {
             const result = JSON.parse(req.responseText);
             this.cost = result.cost;
+            this.gcost = result.gcost;
             this.gold = result.gold;
             this.exp = result.exp;
             this.stages = result.stages;
@@ -146,6 +218,7 @@ export default class App extends Vue {
             this.resultVisible = true;
         };
         req.send(JSON.stringify(assemblePostData(this.materialList, this.extra_outc_bool, this.exp_demand_bool, this.gold_demand_bool)));
+        this.isLoading = false;
     }
 
     public toggleImportOrExport() {
@@ -170,51 +243,50 @@ body
 
 .el-header
     font-size 120%
-    padding-left 15% !important
+    padding-left 5% !important
     font-weight lighter
     color white
     background-color #001429
     width 100%
-    line-height 55px
+    line-height 60px
 
 .el-main
     padding 0 !important
     background-color rgb(236, 245, 255)
+.el-aside
+    padding 0 !important
+    background-color rgb(236, 245, 255)
+    color #001429
 .el-card
-    width 380px
+    width 220px
     display inline-block
-    margin: 10px
+    margin: 5px
 .el-input-number
     margin 5px
 
 .el-card img 
-    width 40px
-    height 40px
+    width 60px
+    height 60px
 
 .el-card.rarity-4
-    border 1px solid #f9cb03
+    border 2px solid #f9cb03
 .el-card.rarity-3
-    border 1px solid #e2b7f3
+    border 2px solid #e2b7f3
 .el-card.rarity-2
-    border 1px solid #1fa8db
+    border 2px solid #1fa8db
 .el-card.rarity-1
-    border 1px solid #d2ef1d
+    border 2px solid #d2ef1d
 .el-card.rarity-0
-    border 1px solid #838383
+    border 2px solid #838383
 
-#analyze
-    position fixed
-    right 30px
-    bottom 30px
-    font-size 40px
-    z-index 100
-    box-shadow 2px 1px 1px rgb(217, 236, 255)
-#importOrExport
-    position fixed
-    right 35px
-    bottom 120px
-    font-size 20px
-    z-index 100
-    box-shadow 2px 1px 1px rgb(217, 236, 255)
+.el-switch__label.is-active
+    color #14314f !important
+.el-switch__label
+    color #88a2bd !important
+
+.el-button--success
+    background-color: #0abab5 !important
+    border-color: #0abab5 !important
+
 
 </style>
